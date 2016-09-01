258f0444f53a543ebec801c66b0a67e0b0597541
Eugeny Dementev <e.dementev@2gis.ru>
2016-09-01 18:25:07 +0700

    Extend console options list by "--bugs-address" option

--- helpers/extract.js
+++ helpers/extract.js
@@ -5,7 +5,7 @@ module.exports = (program, dirs) => {
   shell.exec(`mkdir -p ${program.directory}`);
 
   shell.exec(`python2.7 ${path.join(__dirname, 'extractors/run_babel.py')} extract \
---msgid-bugs-address=lkrteam@2gis.ru \
+--msgid-bugs-address=${program.bugsAddress} \
 --copyright-holder="${program.copyrightHolder}" \
 --keyword=_t:1 --keyword=_nt:1,3 --keyword=_pt:1c,2 --keyword=_npt:1c,2,4 \
 --add-comments=";" \
